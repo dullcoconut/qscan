430612fb6551c5e79edb501fccd96ee1c15c3b30
Jay <jaybunn.jobs@gmail.com>
2024-09-01 16:06:43 +1000

    Update qscan.sh
    better way to define ports

--- qscan.sh
+++ qscan.sh
@@ -2,6 +2,10 @@
 
 read -r -p "ENTER IP= " IP
 
+# DEFINE PORTS HERE------------
+
+ports="-p0-25,80,111,443,445,554,1720-1730,3389,8080"
+
 # Ensure the IP is provided
 if [ -z "$IP" ]; then
   echo "IP address cannot be empty."
@@ -15,9 +19,9 @@ read -r -p "OUTPUT FILENAME= " OUT
 # Check if output provided
 if [ -z "$OUT" ]; then
     clear
-    sudo masscan -p20-25,80,443,445,3389,8080 --wait 1 "$IP"
+    sudo masscan $ports --wait 1 "$IP"
     exit 1
 fi
 
 clear
-sudo masscan -p20-25,80,443,445,3389,8080 --wait 1 "$IP" -oL "$OUT"
+sudo masscan $ports --wait 1 "$IP" -oL "$OUT"
